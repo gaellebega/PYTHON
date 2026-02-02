0018a1940054e2f82cdc4fede32c9f3c550c79fb
gaellebega <gaellebega@gmail.com>
2026-02-03 00:05:30 +0200

    accessing items in the list

--- list.py
+++ list.py
@@ -1,7 +1,12 @@
-list=[
-      [1,2,3],
-      [4,5,6],
-      [7,8,9],
-      [10,11,12]
-    ]
-print(list[:2])
+list = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9],
+  [10,11,12]
+]
+# this is slice method of start and end the start is the -3 and then stop to -1 but just because -1 
+print(list[-3:-1])
+
+# adding an element on the list
+list[1][2]=70
+print(list)
