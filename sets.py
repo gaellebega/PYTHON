c3fe36e87adf7a12e9f161b1419b8acfd286ef31
gaellebega <gaellebega@gmail.com>
2026-02-03 23:02:50 +0200

    tuple

--- sets.py
+++ sets.py
@@ -1,1 +1,25 @@
-# 
+# how do we write sets in python
+
+set1={1,2,3,4,5}
+set2={3,4,5,6,7,8,9,10}
+# print(set1[0])  # this will give error because sets are unordered collections and do not support indexing
+# when we add like that 7 then it is going to be on the last index
+set1.add(9)
+# in sets you can not add already the existing element so there is no error to occur or something but nothing changes
+set1.add(2)
+set1.add(7)
+print(set1)
+set1.clear()
+print(set1)
+# 2.4.26
+set1.update([3, 4, 7])
+print(set1)
+set1.remove(4)
+print(set1)
+# by doing the update using the other set that is to mean like take the set 2 then add some elements of it and then add them to the set1
+set1.update(set2)
+print(set1)
+
+# TUPLES
+tpl=1,2,3,4,5
+print(tpl)
---
+++ tuples.py
@@ -0,0 +1,12 @@
+# TUPLEs
+tpl=1,2,3,4
+tpl2=("q",0,"c",0,9)
+print(type(tpl))
+print(type(tpl2))
+# when we want to make the tuples as one(append) then this is how we can make it
+
+combine=tpl+tpl2
+print(combine)
+print (combine[2])
+print(combine[:3])
+print(combine[3:])
