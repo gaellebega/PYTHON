eb5c59d2f1f63098a35c9b76e3ba6ad1e651c19e
gaellebega <gaellebega@gmail.com>
2026-03-04 20:27:16 +0200

    sorting algorithm

--- DSA-HYTB/dictcomprehension.py
+++ DSA-HYTB/dictcomprehension.py
@@ -1,3 +1,15 @@
+def check_temp(value):
+  if (value>=50):
+    return "No"
+  elif 60>= value >=30:
+    return "Warm"
+  else:
+    return "Cold"
+
 cities={"Gasabo":32,"kicukiro":20,"nyamata":10}
 # key:expression for (key,values) in iterables
-cities={key:() for key,val in cities.items()}
+cities={key:(val-32)*(5/9) for key,val in cities.items()}
+print(cities)
+
+desc_cities={key:check_temp(value) for key,value in  cities.items()}
+print(desc_cities)
---
+++ DSA-HYTB/sorting.py
@@ -0,0 +1,23 @@
+# sorting algorithm 
+# arrangin the data in same logical order
+# increasing or decreasing ordersorting by distribution
+
+# stable sort
+# unstable sort
+# inplace
+# outplace
+# big to small desc
+# comparision sort
+ 
+6,2,8,4,10
+2,6,8,4
+
+# selection sort it is used to pick the smallest one
+
+# you can also sort eitht he largest  but the samll is the mzin thst we use
+# buble sort alwways compare 2 elements
+# selection sort only takes one and comapre it to the others
+
+# INSERTION SORTING
+
+# solving in the half of the problem is the logarithmic time complexity
