db33fd8ced4e47c235aef0ca05035820b05d599c
gaellebega <gaellebega@gmail.com>
2026-03-28 22:49:09 +0200

    python for interview

--- DSA-HYTB/basics.py
+++ DSA-HYTB/basics.py
@@ -1,2 +1,14 @@
 print("march of the new beginning")
 print("*" * 10)
+# PYTHON PEPS
+# varible is assigned to 1
+x = 1
+unit_price = 300
+is_published=False
+# bracket to get the access to the string
+course="beginner in dsa"
+print(course[:])
+
+# escape character this is used to espae
+new="python \"programming"
+print(new)
---
+++ pythonforcoding.py
@@ -0,0 +1,10 @@
+# python is dynamically typed to mean that you dont have 
+n="abc"
+# n has no type the type of n is considered as run time
+# it is okay to have the multiple types in a single line
+
+n,m,z=0.1,"abc",False
+# increment
+n+=1
+n=n+1
+# absense of value it is called none instead of the null in js
