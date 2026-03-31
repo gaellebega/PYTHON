8053a57b65f4530a1b91d13bfffa43e424a6ba68
gaellebega <gaellebega@gmail.com>
2026-03-31 11:51:30 +0200

    boolean

--- boolean.py
+++ boolean.py
@@ -1,6 +1,12 @@
-name=input("Enter your name:")
+# name=input("Enter your name:")
 # name="uwas"
 if name:
   print("yeah that is my name thoo")
 else:
   print("Oops you were so close")
+
+  # any number is True expect 1
+  # \ny number is false except 0
+
+  # when the brackets are not empty we are gonna return True
+bool("hello")
