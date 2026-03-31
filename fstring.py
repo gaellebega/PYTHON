4482c66ad0f026230d543d7aa40aceca0edcab45
gaellebega <gaellebega@gmail.com>
2026-03-31 11:01:13 +0200

    usage of the fsrting

---
+++ fstring.py
@@ -0,0 +1,5 @@
+# we combine the strings using the fstring method in python
+name="Uwimana"
+secondname="Ange"
+age=30
+print(f"\nthis is my name{name} \n and also my second name is {secondname} \n my age is{age}years old\n")
