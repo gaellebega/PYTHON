56d05a5287cc63c89730bc0c48b8b74f4ea2f744
gaellebega <gaellebega@gmail.com>
2026-03-31 11:11:29 +0200

    modifiers

--- fstring.py
+++ fstring.py
@@ -2,4 +2,5 @@
 name="Uwimana"
 secondname="Ange"
 age=30
+# the usage of the fsrting we use the f then we write the braces to input the variabl
 print(f"\nthis is my name{name} \n and also my second name is {secondname} \n my age is{age}years old\n")
---
+++ modifiers.py
@@ -0,0 +1,6 @@
+price=30000
+another=10
+# 2decimal points after our value
+print(f"my ticket cost around {another:.1f}")
+print(f"I bought it att he price of {price:.2f}")
+print(f"the price of my shirt is {200*400} dollars")
