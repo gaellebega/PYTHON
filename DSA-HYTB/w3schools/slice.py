8ce1c2098b2b0d23db65c4beb33dd42d775a4698
gaellebega <gaellebega@gmail.com>
2026-05-04 23:23:56 +0200

    this is the split used

---
+++ DSA-HYTB/w3schools/slice.py
@@ -0,0 +1,11 @@
+name="ISHAMI"
+# take the 3 c
+print(name[:3])
+
+# string modification
+word="Jello World"
+print(word.replace("J","H"))
+
+# to split the string we use split method
+# same as to separate the word using the specific sign 
+print(word.split(","))
