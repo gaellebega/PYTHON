47959593bdad20f9f17b6003121cc3d333077a7c
gaellebega <gaellebega@gmail.com>
2026-05-09 16:04:54 +0200

    lists

---
+++ DSA-HYTB/w3schools/lists.py
@@ -0,0 +1,11 @@
+# to change the lsit intems
+thislist=["apple","banana","orange"]
+thislist[1:3]=["avaocado","toamato"]
+print(thislist)
+
+# we use the insert to insert an element at the specific index
+# we use the append to append the element at the end of the list
+
+# when you want to add the other liek the sets the dictionary on the list then youw ill gonna need ti use the extend method
+
+newlist=["","","",""]
