b9476a2e5069646bda068f4fd9cb0a3fac5d769d
gaellebega <gaellebega@gmail.com>
2026-05-09 20:02:08 +0200

    access the values at the given indexes

--- DSA-HYTB/w3schools/loops.py
+++ DSA-HYTB/w3schools/loops.py
@@ -1,3 +1,16 @@
 thislist=["apple","banana","cherry"]
 for x in thislist:
   print(x)
+
+
+  #you can also use the range and also the len when you want the range of the values in the list
+  # when you want the indexes of the given elements then you are gonna need to use the range and the len together
+
+  for i in range(len(thislist)):
+    print(i) 
+
+
+    #if youw ant to get the valuess of those youa re gonna make something like this
+  for i in range(len(thislist)):
+    # this will print the values at those indexes
+    print(thislist[i]) 
