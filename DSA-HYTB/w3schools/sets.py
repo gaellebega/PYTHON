6cd53a2fd6507944a6ac2f201af09cc24523eb6f
gaellebega <gaellebega@gmail.com>
2026-05-14 02:03:54 +0200

    dicard and remove and pop

--- DSA-HYTB/w3schools/sets.py
+++ DSA-HYTB/w3schools/sets.py
@@ -1,3 +1,8 @@
 # here we are learning the python sets
 thisisset={"apple","banana","cherry"}
+thisisset.update(["umuceri","ibijumba"])
 print(thisisset)
+
+# to add the element to the set we use the update method
+# to remove on the set we can use the reove or discard
+# to remove the random on the set we use the pop()
