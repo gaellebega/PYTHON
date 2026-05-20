74e185de4981d6095736753534faf1bebdc30b1e
gaellebega <gaellebega@gmail.com>
2026-05-20 22:53:29 +0200

    identaion usage

---
+++ DSA-HYTB/w3schools/if.py
@@ -0,0 +1,6 @@
+# in order to use the if statement properly then you have to makethe proper usage of indntation as well
+age=20
+if age>20:
+  print("You are allowed to be married")
+  print("you are allowed to drink")
+  print("you are allowed to live alone")
