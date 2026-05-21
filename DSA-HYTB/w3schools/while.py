e8c9a613ef720435effd9046558daf8ce56f9f0e
gaellebega <gaellebega@gmail.com>
2026-05-21 22:06:26 +0200

    break statement usage

--- DSA-HYTB/w3schools/while.py
+++ DSA-HYTB/w3schools/while.py
@@ -3,3 +3,13 @@ i=6
 while i<2:
   print(i)
   i+=1
+  # when we want to end that condition which is true then we use the break statement
+
+  m=3
+  while m<9:
+    print(i)
+    if m==2:
+      break
+    i+=1
+
+print("this is it")
