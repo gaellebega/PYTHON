f82a72fe97d3345529367182e9f5e89ba1f52d5e
gaellebega <gaellebega@gmail.com>
2026-05-23 22:47:54 +0200

    arbitary arguments

--- DSA-HYTB/w3schools/functions.py
+++ DSA-HYTB/w3schools/functions.py
@@ -7,4 +7,13 @@ my_function()
 # info passed into the function are called the arguments
 def nameslist(fname):
   print("my name is "+fname)
+  # the real value of the variable is called the parameter
 nameslist("Uwase Sangwa")  
+
+# the arbitary arguments
+# it is used when we dont know the number of the the argummets that we are going to use
+
+def makesum_numbers (*numbers):
+  return sum(numbers)
+print(makesum_numbers(1,2,3))
+print(makesum_numbers(1,2))
