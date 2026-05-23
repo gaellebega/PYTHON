35bb03a351abc74cc6d93a36cac56dfa38b6b2d6
gaellebega <gaellebega@gmail.com>
2026-05-23 22:40:40 +0200

    functions with arguments

--- DSA-HYTB/w3schools/functions.py
+++ DSA-HYTB/w3schools/functions.py
@@ -1,3 +1,10 @@
 def my_function():
   print("this is the python function")
 my_function()  
+my_function()
+my_function()
+my_function()
+# info passed into the function are called the arguments
+def nameslist(fname):
+  print("my name is "+fname)
+nameslist("Uwase Sangwa")  
