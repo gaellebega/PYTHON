64ab2c2fc305701c12339d909c1dd874d807dd65
gaellebega <gaellebega@gmail.com>
2026-05-23 22:57:53 +0200

    2 parameters in usage with arbitary args

--- DSA-HYTB/w3schools/functions.py
+++ DSA-HYTB/w3schools/functions.py
@@ -17,3 +17,23 @@ def makesum_numbers (*numbers):
   return sum(numbers)
 print(makesum_numbers(1,2,3))
 print(makesum_numbers(1,2))
+
+# without the *args
+# this will work only for the 2 numbers but whenw e have the alot fo the
+#  numbers thenw e need to make it like the above
+def add(*values):
+  return sum(values)
+print(add(2,3))
+print(add(9,2))
+
+
+def myanotherfunc(*args):
+  print("first argument",args[0])
+  print("second argument",args[1])
+myanotherfunc("Tobias","linus","emilc")  
+  
+def useargs(greeting,*names):
+  # this will go will the number of the names which are 2 then index are 0,1
+  for name in names:
+    print(greeting,name)
+useargs("Hello","Uwimana","Tobias")   
