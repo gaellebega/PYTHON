11832103be56a1dc37119c81dc15405c18a0074c
gaellebega <gaellebega@gmail.com>
2026-05-27 22:19:56 +0200

    grobal in usage

--- DSA-HYTB/w3schools/scope.py
+++ DSA-HYTB/w3schools/scope.py
@@ -1,1 +1,20 @@
 # the variable is only available from the region to which is created which is well know as the scope
+# the nonlocal keyword can be used when we want to make the grobal variable become the local
+
+def myfunc():
+  x="Jane"
+  def func2():
+    nonlocal x
+  func2()
+  return x
+print(myfunc())
+  
+def anotherfunc(x):
+
+  global name
+  name="mukesha"
+  return x
+print(anotherfunc("uwimana"))
+print(name)
+
+
