9716838384b4380e8125a44b2b90c20447dbe234
gaellebega <gaellebega@gmail.com>
2026-05-27 22:26:52 +0200

    decorator

--- DSA-HYTB/w3schools/decorators.py
+++ DSA-HYTB/w3schools/decorators.py
@@ -1,3 +1,20 @@
 # decorators is the way of changing  functions without affecting the orginal codes
 # the gift orginal code
 # the wrapper the decorator
+def decorator(func):
+
+    def wrapper(name):
+        print("Before")
+
+        func(name)
+
+        print("After")
+
+    return wrapper
+
+
+@decorator
+def greet(name):
+    print("Hello", name)
+
+greet("Gaelle")
