7d18b9e68339956e709dc571e0e4c7d3b05ca383
gaellebega <gaellebega@gmail.com>
2026-05-29 16:43:53 +0200

    factorial

--- DSA-HYTB/w3schools/recursion.py
+++ DSA-HYTB/w3schools/recursion.py
@@ -1,9 +1,11 @@
-# the recursion meaans when the function is calling itself
+# the recursion means when the function is calling itself
 def countdown(n):
+  # BASE CASE: CONDITION that sops the function
   if n<=0:
     print("Done")
   else:
     # print(n)
+    # RECURSIVE CASE: EVERY UTPUT AFTER THE RECURSION like the output after function called itself with a changed value
     countdown(n-1)
     print(n)
 countdown(4)
