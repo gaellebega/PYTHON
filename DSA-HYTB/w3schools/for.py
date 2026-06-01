b3db9654598e60b0d03dcf1467c43f4ab1deeb81
gaellebega <gaellebega@gmail.com>
2026-06-01 16:03:04 +0200

    iterable example

--- DSA-HYTB/w3schools/for.py
+++ DSA-HYTB/w3schools/for.py
@@ -1,3 +1,11 @@
 mystring="banana"
 for x in mystring:
   print(x)
+
+  # the iterator usage
+mytuple=("apple","banana","cherry")
+  # for myiter in mytuple print myiter
+myiter=iter(mytuple)
+# we use the next keyword to dispaly the value of the iterables
+# the more we use next the more we display item one by one
+print(next(myiter))
