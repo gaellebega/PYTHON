4b9109f056dc9b90a3ce70d3b0d96b20e94430f6
gaellebega <gaellebega@gmail.com>
2026-06-05 23:23:16 +0200

    helpers examples

--- DSA-HYTB/NEETCODE/helpers.py
+++ DSA-HYTB/NEETCODE/helpers.py
@@ -1,1 +1,9 @@
+import math
 # maths helpers
+# the floor for rounding down
+print(math.floor(10.6))
+# the ceiling for rounding up
+print(math.ceil(30.9))
+print(math.sqrt(100))
+print(2**2)
+print(math.pow(2,2))
