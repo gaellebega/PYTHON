e11a0813cf8fb7edd96fa26360dba3dc5120f690
gaellebega <gaellebega@gmail.com>
2026-06-06 00:20:25 +0200

    insert examples

--- DSA-HYTB/NEETCODE/list.py
+++ DSA-HYTB/NEETCODE/list.py
@@ -7,3 +7,7 @@ print(arr)
 arr.insert(1,7)
 # it is used to insert in the at the specific indes
 # insert(indes,num)
+# it is big of on to insert an element into the list
+# to insert in the middle we can write it like this
+nums=[1,2,3,4]
+nums.insert  (len(nums)//2,10)
